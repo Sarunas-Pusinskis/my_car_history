8418092857c2c9a5d9c9bb32ba2cea903a20c1b0
sharas00 <sarunas.pusinskis@gmail.com>
2023-05-18 00:49:23 +0300

    Ad car Repository

---
+++ src/main/java/lt/sarunas/Spring_demo/my_car_history/My_car_history_Repository.java
@@ -0,0 +1,6 @@
+package lt.sarunas.Spring_demo.my_car_history;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface My_car_history_Repository extends CrudRepository<My_car_history, Integer> {
+}
