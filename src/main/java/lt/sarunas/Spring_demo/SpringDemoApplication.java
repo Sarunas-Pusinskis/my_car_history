236206d02f4e5fed460d1cc1340a43e5bbd09139
sharas00 <sarunas.pusinskis@gmail.com>
2023-05-18 00:38:36 +0300

    Initial Commit

---
+++ src/main/java/lt/sarunas/Spring_demo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package lt.sarunas.Spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
