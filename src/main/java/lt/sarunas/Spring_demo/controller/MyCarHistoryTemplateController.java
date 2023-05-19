c4a716d868c4a4832747cf85618f8722006bbb4e
sharas00 <sarunas.pusinskis@gmail.com>
2023-05-20 00:37:29 +0300

    Implement firstpage_list form with thymeleaf

--- src/main/java/lt/sarunas/Spring_demo/controller/MyCarHistoryTemplateController.java
+++ src/main/java/lt/sarunas/Spring_demo/controller/MyCarHistoryTemplateController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(path = "/mycarhistorytemplate")
 public class MyCarHistoryTemplateController {
@@ -37,5 +39,13 @@ public class MyCarHistoryTemplateController {
         model.addAttribute("service_name", my_car_history.getService_Name());
         model.addAttribute("date", my_car_history.getDate());
             return "/test/firstpage";
+    }
+
+    // http://localhost:8080/mycarhistorytemplate/firstpage/all
+    @GetMapping(path = "/firstpage/all")
+    public String getAllMyCarHistoryFirstPageList (Model model){
+        List<My_car_history> my_car_histories =  myCarHistoryService.getAllMyCarHistory();
+        model.addAttribute("key_my_car_history_list", my_car_histories);
+        return "/test/firstpage_mycarhistory_list";
     }
 }
