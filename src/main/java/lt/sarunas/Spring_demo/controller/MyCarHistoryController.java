0f70e9eea50a0db50eaaebbd82f259d68a11eb79
sharas00 <sarunas.pusinskis@gmail.com>
2023-06-17 17:47:04 +0300

    Made shorter links

--- src/main/java/lt/sarunas/Spring_demo/controller/MyCarHistoryController.java
+++ src/main/java/lt/sarunas/Spring_demo/controller/MyCarHistoryController.java
@@ -26,46 +26,46 @@ public class MyCarHistoryController {
         return "home_page";
     }
 
-    // http://localhost:8080/mycarhistory/mycarhistory/1
-    @GetMapping(path = "/mycarhistory/{id}")
+    // http://localhost:8080/mycarhistory/1
+    @GetMapping(path = "/{id}")
     public String getMyCarHistory(@PathVariable int id, Model model) {
         CarHistory carHistory = myCarHistoryService.getMy_car_historyById(id);
         model.addAttribute("myCarHistory", carHistory);
         return "/mycarhistory/mycarhistory_th";
     }
 
-    // http://localhost:8080/mycarhistory/mycarhistory/all1
-    @GetMapping(path = "/mycarhistory/all1")
+    // http://localhost:8080/mycarhistory/all1
+    @GetMapping(path = "/all1")
     public String getAllMyCarHistory(Model model) {
         List<CarHistory> allMyCarHistory = myCarHistoryService.getAllMyCarHistory();
         model.addAttribute("myCarHistories", allMyCarHistory);
         return "/mycarhistory/mycarhistories_th";
     }
 
-    // http://localhost:8080/mycarhistory/mycarhistory/getandpost
-    @GetMapping(value = "/mycarhistory/getandpost")
+    // http://localhost:8080/mycarhistory/getandpost
+    @GetMapping(value = "/getandpost")
     public String getMyCarHistoryByName(Model model) {
         model.addAttribute("myCarHistory", new CarHistory());
         model.addAttribute("myCarHistories", Collections.emptyList());
         return "/mycarhistory/post_get_mycarhistories_th";
     }
 
-    @PostMapping(value = "/mycarhistory/getandpost")
+    @PostMapping(value = "/getandpost")
     public String postMyCarHistoryByNameLike(Model model, @ModelAttribute(value = "myCarHistory") CarHistory carHistory) {
         List<CarHistory> myCarHistories = myCarHistoryService.getMyCarHistoryByNameLike(carHistory.getPart_Name());
         model.addAttribute("myCarHistories", myCarHistories);
         return "/mycarhistory/post_get_mycarhistories_th";
     }
 
-    // http://localhost:8080/mycarhistory/mycarhistory/expenses
-    @GetMapping(value = "/mycarhistory/expenses")
+    // http://localhost:8080/mycarhistory/expenses
+    @GetMapping(value = "/expenses")
     public String getCarExpenses(Model model) {
         CarExpenses carExpenses = myCarHistoryService.getMyCarExpenses();
         model.addAttribute("carExpenses", carExpenses);
         return "/mycarhistory/carExpenses";
     }
 
-    @PostMapping(value = "/mycarhistory/add-new-expenses")
+    @PostMapping(value = "/add-new-expenses")
     public String addNewExpenses(Model model, @ModelAttribute(value = "myCarHistory") CarHistory carHistory) {
         myCarHistoryService.insertNewExpense(carHistory);
         List<CarHistory> allMyCarHistory = myCarHistoryService.getAllMyCarHistory();
@@ -74,7 +74,8 @@ public class MyCarHistoryController {
         return "/mycarhistory/add_mycarhistories_th";
     }
 
-    @DeleteMapping(value = "/mycarhistory/delete-expenses")
+    // http://localhost:8080/mycarhistory/delete-expenses
+    @DeleteMapping(value = "/delete-expenses")
     public String deleteExpenses(Model model, @ModelAttribute(value = "myCarHistory") CarHistory carHistory) {
         myCarHistoryService.deleteHistoryEntry(carHistory.getId());
         List<CarHistory> allMyCarHistory = myCarHistoryService.getAllMyCarHistory();
@@ -83,8 +84,8 @@ public class MyCarHistoryController {
         return "/mycarhistory/add_mycarhistories_th";
     }
 
-    // http://localhost:8080/mycarhistory/mycarhistory/all
-    @GetMapping(path = "/mycarhistory/all")
+    // http://localhost:8080/mycarhistory/all
+    @GetMapping(path = "/all")
     public String getAllExpenses(Model model) {
         List<CarHistory> allMyCarHistory = myCarHistoryService.getAllMyCarHistory();
         model.addAttribute("myCarHistories", allMyCarHistory);
